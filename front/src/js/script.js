dd94cc82ac96838508fbac303c82e7bb1a717c9c
kyogo <kyogo@kyogray.local>
2022-09-15 11:27:35 +0900

    地図埋め込みはしたyo

--- front/src/js/script.js
+++ front/src/js/script.js
@@ -1,4 +1,38 @@
 'use strict';
 
 
-console.log(1111);
+
+var map;
+var marker;
+var center = {
+  lat: 34.7019399, // 緯度
+  lng: 135.51002519999997 // 経度
+};
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+     center: center, // 地図の中心を指定
+     zoom: 19 // 地図のズームを指定
+  });
+
+  marker = new google.maps.Marker({ // マーカーの追加
+      position: center, // マーカーを立てる位置を指定
+      map: map // マーカーを立てる地図を指定
+  });
+}
+
+
+fetch('https://gi3wuc5qt1.execute-api.ap-northeast-1.amazonaws.com/test/', {
+    method: "GET",
+    mode: 'cors'
+  })
+  .then((response) => {
+    if (response.ok) {
+      return response.json().then(resJson => {
+        console.log(JSON.stringify(resJson));
+      });
+    }
+    throw new Error('Network response was not ok.');
+  })
+  .catch(error => {
+    console.error(error);
+  })
