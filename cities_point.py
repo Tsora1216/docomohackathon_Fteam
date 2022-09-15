efa032ac46f43e391a67c6f57d954a92f07a22c8
KotaYoshikawa <clown.66137@gmail.com>
2022-09-15 15:03:43 +0900

    first commit

---
+++ cities_point.py
@@ -0,0 +1,7 @@
+import requests
+def cities_point():
+  geo_request_url = 'https://get.geojs.io/v1/ip/geo.json'
+  data = requests.get(geo_request_url).json()
+  #print(data['latitude'])
+  #print(data['longitude'])
+  return data['latitude'],data['longitude']
